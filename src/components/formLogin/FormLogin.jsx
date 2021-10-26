1f736700e2405924eaf3a8098cbeb926d2d7f712
Andre Hugo Bastos da Silva <andre.hugo.b.silva@accenture.com>
2021-10-26 18:44:49 -0300

    criando eventos formLogin

--- src/components/formLogin/FormLogin.jsx
+++ src/components/formLogin/FormLogin.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react"
-import Button from "../button/Button";
 
 export default function FormLogin(props) {
 
   const [isLogado, setIsLogado] = useState(true);
 
+  let usuario = {
+    "nickname": "",
+    "email": "",
+    "senha": "",
+    "dataNascimento": new Date().toLocaleDateString('pr-br')
+  }
+
+  let convidado
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
 
   return(
-    <form>
-    <h2>{props.header}</h2>
-    {props.isConvidado ?
-      <input type="text" placeholder="Nickname"></input>
-     : <input type="text" placeholder="E-mail"></input>
-      <input type="password" placeholder="Senha"></input> }
-     <Button> Entrar </Button>
+    <form onSubmit={handleSubmit}>
+      <h2>{props.header}</h2>
+      {props.isConvidado ?
+        <>
+          <input type="text" placeholder="Nickname"></input>
+        </>
+      : <>
+          <input type="text" placeholder="E-mail"></input>
+          <input type="password" placeholder="Senha"></input> 
+        </>
+      }
+      <input type="submit" value="Entrar" />
     </form>
   )
 }
